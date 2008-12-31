4291f6a87fd395dba54df4db380f8e82a73494a1
Jay Phillips <jicksta+git@gmail.com>
2008-12-30 20:17:43 -0800

    Started migration to Rails

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,51 @@
+class PageController < ApplicationController
+  
+  def architecture
+    @title = "Architecture Overview"
+  end
+  
+  def business_case
+    @title = "The Business Case"
+  end
+  
+  def consulting
+    @title = "Consulting"
+  end
+  
+  def contact
+    @title = "Contact Us"
+  end
+  
+  def contributing
+    @title = "Contributing"
+  end
+  
+  def download
+    @title = "Download Adhearsion"
+  end
+  
+  def examples
+    @title = "Examples"
+  end
+  
+  def faq
+    @title = "Frequently Asked Questions"
+  end
+  
+  def getting_started
+    @title = "Getting Started"
+  end
+  
+  def irc
+    @title = "IRC Chatroom"
+  end
+  
+  def roadmap
+    @title = "Roadmap"
+  end
+  
+  def screencasts
+    @title = "Screencasts"
+  end
+  
+end
