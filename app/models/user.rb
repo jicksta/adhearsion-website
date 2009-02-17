7e417db38b2d01d4ca23b32e69e070fea6f640fc
Jay Phillips <jicksta+git@gmail.com>
2009-02-17 02:47:15 -0800

    New accounts can now specify a skype username

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,10 @@ class User < ActiveRecord::Base
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  
-
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :skype
 
 
   def admin?
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090217041800) do
+ActiveRecord::Schema.define(:version => 20090217102442) do
 
   create_table "example_sections", :force => true do |t|
     t.integer  "position"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20090217041800) do
     t.datetime "activated_at"
     t.string   "state",                                    :default => "passive"
     t.datetime "deleted_at"
+    t.string   "skype"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
