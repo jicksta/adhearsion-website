ecee443429785fb191bfd539d7aecd932713a19b
Jay Phillips <jicksta+git@gmail.com>
2009-04-26 21:50:13 -0700

    Adding sandbox_test

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,18 @@ class ApiController < ApplicationController
     
   end
   
+  def sandbox_test
+    socket = TCPSocket.new('localhost', 20_000)
+    socket.sync = true
+
+    socket.print "agi_sandbox: testing123\n\n"
+    socket.print "200 result=1 (sandbox_test)\n"
+    socket.print "SANDBOX TEST\n"
+    socket.close
+    
+    render :text => "sent"
+  end
+  
   def username_from_md5
     identifier_hash = params[:identifier_hash]
     if identifier_hash
